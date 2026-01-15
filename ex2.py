418ac89c4706b0b9fb59c49c1c76455eda188f96
vansh lakhwani <lakhwanivansh@gmail.com>
2026-01-15 22:12:39 +0530

    C10 : Added substraction in ex2.py

--- ex2.py
+++ ex2.py
@@ -2,7 +2,13 @@ def division(a,b):
     return a/b
 
 def modulus(a,b):
-    return a
+    return a%b
 
 def multiplication(a,b):
     return a*b
+
+def substraction(a,b):
+    return a-b
+
+def addition(a,b):
+    return a+b
